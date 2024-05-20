e2089b959ad1d0174b0c4d0054d205a7e0449cab
shauniif <francescavescio12@gmail.com>
2024-05-20 16:00:46 +0200

    -

--- Esercizio S9-L1/script.js
+++ Esercizio S9-L1/script.js
@@ -1,26 +1,44 @@
-"use strict";
-class User {
-    constructor(nome, cognome) {
+var User = /** @class */ (function () {
+    function User(nome, cognome) {
         this.nome = nome;
         this.cognome = cognome;
         this.credito = 0;
         this.numeroChiamate = 0;
     }
-    ricarica(importo) {
+    User.prototype.ricarica = function (importo) {
         this.credito += importo;
-        console.log(`${this.nome} + ${this.cognome} hai effettuato una ricarica di ${importo}.`);
+        console.log("".concat(this.nome, " ").concat(this.cognome, ": hai effettuato una ricarica di ").concat(importo, "."));
         return this.credito;
-    }
-    chiamata(minuti) {
-        const costoChiamataalMinuto = 0.20;
-        const costoTotaleDellaChiamata = costoChiamataalMinuto * minuti;
-        if (this.credito > costoTotaleDellaChiamata)
+    };
+    User.prototype.chiamata = function (minuti) {
+        var costoChiamataalMinuto = 0.2;
+        var costoTotaleDellaChiamata = costoChiamataalMinuto * minuti;
+        if (this.credito > costoTotaleDellaChiamata) {
             this.credito -= costoTotaleDellaChiamata;
-        this.numeroChiamate++;
-        console.log();
-        return costoTotaleDellaChiamata;
-    }
-}
-const user = new User('Mario', 'Rossi');
+            this.numeroChiamate += minuti;
+            console.log("La chiamata effettuata \u00E8 durata ".concat(minuti, ". Costo della chiamata: ").concat(costoTotaleDellaChiamata, ". Credito residuo: ").concat(this.credito));
+        }
+        else {
+            console.log('Impossibile effettuare la chiamata. Credito insufficiente');
+        }
+        return this.credito;
+    };
+    User.prototype.chiama404 = function () {
+        console.log("Credito attuale: ".concat(this.credito));
+        return this.credito;
+    };
+    User.prototype.getNumeroChiamata = function () {
+        console.log("Hai effettuato ".concat(this.numeroChiamate, " minuti di chiamate"));
+        return this.numeroChiamate;
+    };
+    User.prototype.azzeraChiamate = function () {
+        this.numeroChiamate = 0;
+    };
+    return User;
+}());
+var user = new User('Mario', 'Rossi');
 console.log(user);
 user.ricarica(5);
+user.chiamata(12);
+user.chiama404();
+user.getNumeroChiamata();
--- Esercizio S9-L1/script.ts
+++ Esercizio S9-L1/script.ts
@@ -1,8 +1,9 @@
 interface iManageSIM{
     ricarica(sum:number): number,
     chiamata(minuti:number): number,
-   // chiama404(): number;
-   // getNumeroChiamata(): number;
+    chiama404(): number;
+    getNumeroChiamata(): number;
+    azzeraChiamate(): void
 }
 
 class User implements iManageSIM {
@@ -18,29 +19,39 @@ class User implements iManageSIM {
     }
     ricarica(importo: number):number {
         this.credito += importo
-        console.log(`${this.nome} + ${this.cognome} hai effettuato una ricarica di ${importo}.` )
+        console.log(`${this.nome} ${this.cognome}: hai effettuato una ricarica di ${importo}.` )
         return this.credito;
     }
     chiamata(minuti: number):number {
-        const costoChiamataalMinuto = 0.20;
+        const costoChiamataalMinuto = 0.2;
         const costoTotaleDellaChiamata = costoChiamataalMinuto * minuti
-
-        if(this.credito > costoTotaleDellaChiamata)
-        this.credito -= costoTotaleDellaChiamata
-        this.numeroChiamate++
-        console.log()
-        return costoTotaleDellaChiamata;
-        } 
         
-
+        if(this.credito > costoTotaleDellaChiamata) {
+            this.credito -= costoTotaleDellaChiamata
+            this.numeroChiamate += minuti
+            console.log(`La chiamata effettuata è durata ${minuti}. Costo della chiamata: ${costoTotaleDellaChiamata}. Credito residuo: ${this.credito}`)
+        } else {
+            console.log('Impossibile effettuare la chiamata. Credito insufficiente')
+        }
+        return this.credito
     }
-  /*  chiama404(): number {
-        throw new Error("Method not implemented.");
-    } */
-/*    getNumeroChiamata(): number {
-        throw new Error("Method not implemented.");
-    } */
+    chiama404(): number {
+        console.log(`Credito attuale: ${this.credito}`);
+        return this.credito
+    } 
+    getNumeroChiamata(): number {
+        console.log(`Hai effettuato ${this.numeroChiamate} minuti di chiamate`)
+        return this.numeroChiamate
+    } 
+    azzeraChiamate(): void {
+        this.numeroChiamate = 0;        
+    }
+
+}
 
 const user = new User('Mario', 'Rossi') 
 console.log(user)
 user.ricarica(5)
+user.chiamata(12)
+user.chiama404()
+user.getNumeroChiamata()
